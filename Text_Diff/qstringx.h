149493ce723e0b8b5414f63c0bf656bdaf3d723f
cdplusLIU <liuwb980@gmail.com>
2014-04-12 14:33:56 +0800

    添加 uibase测试用

--- Text_Diff/qstringx.h
+++ Text_Diff/qstringx.h
@@ -12,7 +12,14 @@ public:
 public:
     static QString String_Common(const QString& strA,const QString& strB);
     static QString String_Fill(const QString& strA,const QString& strB);
-    static QString String_Diff(const QString& strA,const QString& strB);
+    QString String_Diff(const QString& strA,const QString& strB);
+
+public:
+    QString GetDiffRes(void) const;
+    void InitDiffRes(const QString& strRes);
+
+private:
+    QString m_strDiffRes;
 };
 
 #endif // QSTRINGX_H
